89191fc474b3bda49ed6868a92373c457b98b1cc
EnricoGiovanno8 <genrico189@gmail.com>
2024-09-06 15:29:51 +0700

    add testimonials section

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
+import Icons from './Icons';
 import Image from './Image';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import Navbar from './Navbar';
 import Phone from './Phone';
 
-export { Image, MaxWidthWrapper, Navbar, Phone };
+export { Icons, Image, MaxWidthWrapper, Navbar, Phone };
