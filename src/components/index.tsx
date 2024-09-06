ddad3109d8b0778d5752f4faf11ec7c05da91722
EnricoGiovanno8 <genrico189@gmail.com>
2024-09-06 15:17:22 +0700

    add navbar and user authentication

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Image from './Image';
 import MaxWidthWrapper from './MaxWidthWrapper';
+import Navbar from './Navbar';
 import Phone from './Phone';
 
-export { Image, MaxWidthWrapper, Phone };
+export { Image, MaxWidthWrapper, Navbar, Phone };
